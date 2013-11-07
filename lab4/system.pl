6238687892ca280b18dc681408fb7284e99dc8ab
Brian Jacobel <bjacobel@gmail.com>
2013-11-07 14:33:10 -0500

    First part of Lab 4 done

--- lab4/system.pl
+++ lab4/system.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/perl
 
-$top_output = `top -n 5 -o mem -l 1`;
-#$du_output = `du -shc /Users/*`
+############################
+### DO THE PROCESS STATS ###
+############################
 
+$top_output = `top -n 5 -o mem -l 1`;
 @to = split("\n\n", $top_output);
 @processes = split("\n", $to[1]);
 @column_heads = split(/\s+/, $processes[0]);
@@ -22,16 +24,66 @@ foreach $column_header (@column_heads) {
     $i += 1;
 }
 
-print "-- Process summary -- ", "\n";
+$mail_content = sprintf "-- Process summary -- \n";
 
 foreach $process (@processes[1..5]){
     # Replace every space between two words with an underscore, so we don't break up multiword process names and throw off our array indicies
     # BRO DO YOU EVEN REGEX
-    $process =~ s/(?<=[a-z])(\s+)(?=[A-Z])/\_/;
+    $process =~ s/(?<=[a-z])(\s+)(?=[A-Z])/\_/g;
     @stats = split(/\s+/, $process);
     
     # then put the space back for pretty printing
-    $stats[$command_offset] =~ s/\_/\ /;
+    $stats[$command_offset] =~ s/\_/\ /g;
+
+    # Format the mem usage up a little
+    $stats[$mem_offset] =~ s/M\+//;
+
+    if ($stats[$mem_offset] > 750){
+        $alert = " (ALERT: over 750MB!!)";
+    } else {
+        $alert = "";
+    }
     
-    printf"%s: process '%s' (PID %d), started by user %s\n",$stats[$mem_offset],$stats[$command_offset],$stats[$pid_offset],$stats[$user_offset];
+    $mail_content .= sprintf "Process '%s' (PID %d) is using %s MB RAM, owner: %s%s\n",$stats[$command_offset],$stats[$pid_offset],$stats[$mem_offset],$stats[$user_offset],$alert;
 }
+
+############################
+### DO THE DISKUSE STATS ###
+############################
+
+#print "The 'du' command needs your sudo password. Also, please note that it may take some time to run.\n"; 
+#$du_output = `sudo du -hms /Users/*`;
+#@diskuses = split("\n", $du_output);
+
+$input_file = "/Users/bjacobel/Documents/sem7/proglang/code/lab4/du";
+open INPUT_FILE, "<$input_file"; 
+@diskuses = <INPUT_FILE>;
+
+$mail_content .= sprintf "\n-- Disk use summary -- \n";
+foreach $diskuse (@diskuses) {
+    @stats = split(/(?<=\d)(\s+)(?=\/)/, $diskuse);
+    $stats[2] =~ s/\/Users\///;
+    $stats[2] =~ s/\n//;
+    if ($stats[0] > 10240){
+        $alarm = " (ALERT: over 10 GB!!)";
+    } else {
+        $alarm = "";
+    }
+    $mail_content .= sprintf "User %s is using %s MB of disk space%s.\n", $stats[2], $stats[0], $alarm;
+}
+
+#####################
+### MAIL THAT ISH ###
+#####################
+
+$to = "bjacobel\@bowdoin.edu";
+
+open(MAIL, "|mail -s 'System Stats' $to");
+print MAIL $mail_content;
+close(MAIL);
+
+print $mail_content;
+print "\nA copy of this report was mailed to $to.", "\n";
+
+
+
