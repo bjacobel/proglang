6cab9ad36e553fcb6a1abe24abf61171a5b9bc51
Brian Jacobel <bjacobel@gmail.com>
2013-11-19 00:52:30 -0500

    Damn you rate limiting!!

---
+++ lab6/twitter.rb
@@ -0,0 +1,40 @@
+require 'twitter'
+
+Twitter.configure do |config|
+    config.consumer_key = "xaD9NI779fFhx5EpegCwbw"
+    config.consumer_secret = "QiRYthjNH0dEMP1kBDTlVfuUKjRrYi90ZwH1B9t9Q"
+    config.oauth_token = "15299776-qbzGy4hqag1xGVL4YEzaI0Pu27uqPvVEuDGslnN74"
+    config.oauth_token_secret = "Yt78nOxoOcqgqgvAWKJ1BTjkfkhkiCUQ5vXiTShMTe6v5"
+end
+
+$users = [] 
+
+def find_friends(user, depth)
+    if depth > 5
+        puts "Maximum depth reached."
+        return
+    else
+        new_friends = []
+        
+        begin
+            Twitter.friends(user).each do |friend|
+               new_friends << friend.name if !$users.include?(friend.name)
+            end
+        rescue Twitter::Error::TooManyRequests => error
+            puts "Rate limit exceeded. Sleeping it off."
+            puts "$users currently has #{$users.length} entries."
+            sleep (error.rate_limit.reset_in + 60)
+            retry
+        end
+
+        new_friends.each do |friend| 
+            find_friends(friend, depth + 1)
+        end
+    end
+end
+
+firstuser = "bjacobel"
+$users << firstuser
+find_friends(firstuser, 0)
+
+puts users
